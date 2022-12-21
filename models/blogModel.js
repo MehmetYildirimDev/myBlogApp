6d76dfc16cde01a3ffeed7bed208b87b079bf155
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-12-21 18:38:15 +0300

    feat: create blog modul and sitemodul

---
+++ models/blogModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const blogSchmea = new mongoose.Schema({
+
+    title            : {type: String, required: "Cannot be empty",},
+    comSentence      : {type: String, required: "Cannot be empty",},
+    comImage         : {type: String, required: "Cannot be empty",},
+    blog             : {type: String, required: "Cannot be empty",},
+    date             : {type:   Date, default : Date.now,},
+
+});
+
+module.exports = mongoose.model("Blog", blogSchmea);
