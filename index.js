1cd9fd3067adaafd194a7c6c409f99419e4e8927
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-12-21 18:54:54 +0300

    feat: Adding Session Property for Login

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const mongoose = require("mongoose"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    app = express();
+const mongoose      =    require("mongoose"),
+    express         =    require("express"),
+    passport        =    require("passport"),
+    LocalStragety   =    require("passport-local"),
+    User            =    require("./models/usersModel"),
+    expressSession  =    require("express-session"),
+    bodyParser      =    require("body-parser"),
+    app             =    express();
 
 //Routes
 const indexRoutes = require("./routes/indexRoutes");
       adminRoutes = require("./routes/adminRoutes");
 
 //appConfig
+mongoose.connect("mongodb://localhost/BlogApp");
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+//passport config
+app.use(require("express-session")({
+    secret:"bu bizim guvenlik cumlemiz",
+    resave:false,
+    saveUninitialized: false,
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStorage(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //routes using 
 app.use(indexRoutes);
 app.use(adminRoutes);
+
+
 
 const server = app.listen(3000, (err) => {
     if (err){
